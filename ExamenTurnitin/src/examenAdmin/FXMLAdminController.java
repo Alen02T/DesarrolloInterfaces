520f15d8e0e4573dca9647e125fd86ca25cf6158
Alen02T <gameralen2002@gmail.com>
2022-01-31 01:07:27 +0100

    TurnitinDefinitivo

--- ExamenTurnitin/src/examenAdmin/FXMLAdminController.java
+++ ExamenTurnitin/src/examenAdmin/FXMLAdminController.java
@@ -63,149 +63,131 @@ public class FXMLAdminController implements Initializable {
     @FXML
     private TilePane tlPane;
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        tlPane.getChildren().clear();
-        
+    
+    public void dibujar(){
         File path = new File(Constants.path);
         File[] archivos = path.listFiles();
         
-        
-         
         for(int i=0;i<archivos.length;i++){
-            int numeroTotal=0;
-            //System.out.println(archivos[i].getName());
-            String archivo1 = leer(archivos[i].getName());
-             
-             for(int j=0;j<archivos.length;j++){
+            
+            
+            String archivo1 = leerArchivo(archivos[i].getName());
+            
+            int suma=0;
+            
+            for(int j=0;j<archivos.length;j++){
+                if(i!=j){
+                    String archivo2 = leerArchivo(archivos[j].getName());   
+                    int resultado = Constants.compareStringsPercentage(archivo1, archivo2);
+                    suma = suma + resultado;   
+                }
+            }
                 
-                 if(j!=i){
-                    String archivo2 = leer(archivos[j].getName());
-                    int comparacionNumerica = compareStringsPercentage(archivo1, archivo2);
-                    //System.out.println("Comparacion entre " + archivos[i].getName() + " " + archivos[j].getName() + " = " + comparacionNumerica);
-                    numeroTotal+=comparacionNumerica;  
-                 }
-             }
-             
-            int  numeroFinal = numeroTotal/(archivos.length-1);
-            String numeroFinalTexto = String.valueOf(numeroFinal);
-            
-            
+            int resultadoTotal = suma/(archivos.length-1);
             Text texto = new Text();
             texto.setText(archivos[i].getName());
             Text texto2 = new Text();
-            texto2.setText(numeroFinalTexto+"%");
+            
+            if(resultadoTotal>60){
+                
+                texto2.setFont(Font.font("verdana",FontWeight.BOLD,FontPosture.REGULAR,30));
+                texto2.setFill(Color.RED);
+                texto2.setText(resultadoTotal+"%");
+            }else{
+                texto2.setText(resultadoTotal+"%");
+            }
+            
             BorderPane borde = new BorderPane();
             borde.setCenter(texto2);
             borde.setBottom(texto);
             borde.setStyle("-fx-border-color:black");
             borde.setPadding(new Insets(10, 20, 10, 20));
-            
-            
-            if(numeroFinal>45){
-                texto2.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
-                texto2.setFill(Color.RED);
-            }
-            
             tlPane.getChildren().addAll(borde);
-                    borde.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            
+            borde.setOnMouseClicked(new EventHandler<MouseEvent>() {
                       @Override
                     public void handle(MouseEvent mouseEvent) {
                         if(mouseEvent.getButton().equals(MouseButton.PRIMARY)){
                             if(mouseEvent.getClickCount() == 2){
-                                final Stage dialog = new Stage();
-                                dialog.initModality(Modality.WINDOW_MODAL);
-                                //Stage primaryStage = (Stage) fieldJugadorUno.getScene().getWindow();
-                                //dialog.initOwner(primaryStage);
-                                VBox dialogVbox = new VBox(20);
-                                dialogVbox.getChildren().add(new Text("Propiedades del archivo"));
-                                dialogVbox.getChildren().add(new Text("Nombre del archivo: " + texto.getText()));
-                                dialogVbox.getChildren().add(new Text("Nombre del propietario: " + Constants.User));
-                                dialogVbox.getChildren().add(new Text("Porcentaje Total de Similaridad: " + texto2.getText()));
-                                Scene dialogScene = new Scene(dialogVbox, 300, 200);
-                                dialog.setScene(dialogScene);
-                                dialog.show();
-                                return;
+                                
+                              String nombreSeleccionado=texto.getText();
+                              System.out.println(nombreSeleccionado);
+                           
+                                       String archivo1 = leerArchivo(nombreSeleccionado);
 
+                                       
+
+                                       for(int j=0;j<archivos.length;j++){
+                                           if(!nombreSeleccionado.equals(archivos[j].getName())){
+                                               System.out.println("Archivos no seleccionados: " + archivos[j].getName());
+                                               String archivo2 = leerArchivo(archivos[j].getName());   
+                                               int resultado = Constants.compareStringsPercentage(archivo1, archivo2);
+                                               
+                                               System.out.println(nombreSeleccionado + " vs " + archivos[j].getName() + " = " + resultado);
+                                               
+                                               if(resultado>Constants.valorMasAlto){
+                                                   
+                                                   Constants.valorMasAlto=resultado;
+                                                   Constants.nombreArchivo=archivos[j].getName();
+                                               }
+                                           }
+                                       }
+                            
+                            
+                                       final Stage dialog = new Stage();
+                                        dialog.initModality(Modality.WINDOW_MODAL);
+                                        VBox dialogVbox = new VBox(20);
+                                        dialogVbox.getChildren().add(new Text("Propiedades del archivo"));
+                                        dialogVbox.getChildren().add(new Text("Nombre del archivo: " +  Constants.nombreArchivo));
+                                        dialogVbox.getChildren().add(new Text("Nombre del propietario: " + Constants.user));
+                                        dialogVbox.getChildren().add(new Text("Porcentaje Total de Similaridad: " + Constants.valorMasAlto+"%"));
+                                        Constants.valorMasAlto=0;
+                                        Constants.nombreArchivo="";
+                                        Scene dialogScene = new Scene(dialogVbox, 300, 200);
+                                        dialog.setScene(dialogScene);
+                                        dialog.show();
+                                        return;      
+                                       
                             }
                         }
                     }
-    });
+                    
+            });
+            
             
         }
         
-
-        
-        
-        
-
-
     }
     
     
-    
-    
-    
-       public static int compareStringsPercentage(String s1, String s2) {
-        int percentage = 0;
-
-        int total = 0;
-        int fullMatch = 0;
-        // Check for each character at same location
-        total += charMatch(s1, s2);
-        fullMatch += charMatch(s1, s1);
-
-        // Calc percentage
-        percentage = (int) Math.round(total / (fullMatch / 100.0));
-        return percentage;
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        dibujar();
     }
-
-    /**
-     * Check how many characters of string1 are in the same location as string2
-     * @param s1
-     * @param s2
-     * @return
-     */
-    private static int charMatch(String s1, String s2) {
-        char[] as1 = s1.toCharArray();
-        char[] as2 = s2.toCharArray();
-        int match = 0;
-        for (int i = 0; i < as1.length; i++) {
-            char c = as1[i];
-            if (i < as2.length) {
-                if (as2[i] == c) {
-                    match++;
+    
+    
+       public static String leerArchivo(String nombre){
+            
+            
+            try(BufferedReader br = new BufferedReader(new FileReader(Constants.path+"/"+nombre))) {
+                    StringBuilder sb = new StringBuilder();
+                    String line = br.readLine();
+                    while (line != null) {
+                        sb.append(line);
+                        sb.append(System.lineSeparator());
+                        line = br.readLine();
+                    }
+                    String everything = sb.toString();
+                    return everything;
+                } catch (IOException ex) {
+                   ex.printStackTrace();
                 }
-            }
+        return null;
+            
+          }
 
-        }
-        return match;
-    }
     
     
-    public static String leer(String nombre){
-    
-      try(BufferedReader br = new BufferedReader(new FileReader(Constants.path+"/"+nombre))) {
-                                    StringBuilder sb = new StringBuilder();
-                                    String line = br.readLine();
-
-                                    while (line != null) {
-                                        sb.append(line);
-                                        sb.append(System.lineSeparator());
-                                        line = br.readLine();
-                                    }
-                                    
-                                    
-                                    
-                                    String everything = sb.toString();
-                                    return everything;
-                                    
-                                } catch (IOException ex) {
-                                    //Logger.getLogger(.class.getName()).log(Level.SEVERE, null, ex);
-                                    
-                                }
-       return null; 
-     
-    }
+  
 }
 
